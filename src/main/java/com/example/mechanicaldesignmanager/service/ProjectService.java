164ffba8743fd7d873106546772a4ac3771fe896
AndrijKostecki <kostecki.andrij@gmail.com>
2026-01-18 09:08:41 +0100

    Unnecessary files were deleted

--- src/main/java/com/example/mechanicaldesignmanager/service/ProjectService.java
+++
@@ -1,4 +0,0 @@
-package com.example.mechanicaldesignmanager.service;
-
-public class ProjectService {
-}
--- src/main/java/com/example/mechanicaldesignmanager/service/UnitService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mechanicaldesignmanager.service;
-
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-
-public class UnitService {
-
-}
